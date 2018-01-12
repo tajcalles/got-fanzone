347b43db544005b2c317b2337efd29b88135bcab
Taj Calles <tajcalles@gmail.com>
2018-01-12 11:01:09 -0700

    updated location

--- src/components/JoinUs.js
+++ src/components/JoinUs.js
@@ -7,9 +7,9 @@ class JoinUs extends Component {
     return (
       <div>
         <Container>
-          <Header as='h1' textAlign='center'>Find a GOT Meetup</Header>
+          <Header as='h1' textAlign='center'>JOIN US</Header>
           <Header as='h3' textAlign='left'>Location</Header>
-          <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1511.0303314710347!2d-111.88428610095525!3d40.76069047493472!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8752f5130c207e95%3A0x7d24574a9ed4d540!2sCharlie+Chows+Dragon+Grill!5e0!3m2!1sen!2sus!4v1515279306579" width="300" height="250" frameborder="0" style={{ border: "0" }} allowfullscreen></iframe>
+          <iframe src="https://www.google.com/maps/place/Iceland/@64.8013826,-23.7302259,6z/data=!3m1!4b1!4m5!3m4!1s0x48d22b52a3eb6043:0x6f8a0434e5c1459a!8m2!3d64.963051!4d-19.020835" width="300" height="250" frameborder="0" allowfullscreen></iframe>
         </Container>
       </div>
     );
